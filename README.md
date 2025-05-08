43e3c385f185c0bf9394770ddc664183289a7b54
Electroman179 <jameschaowadee@gmail.com>
2025-05-08 10:15:49 +0700

    update all

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my test of lazygit this repo will not do anything
--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import time
 
+start_time = time.time()
 print("hello world")
 print(f"time in time.time format: {time.time()}")
+end_time = time.time()
+
+run_time = end_time - start_time
+
+print(f"used {run_time:.4f} seconds to run")
+
